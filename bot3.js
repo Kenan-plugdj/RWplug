726162ba55df9bd1604aeb9b0a73b20aee5000f7
Kenan-plugdj <marlons.r.davila@gmail.com>
2016-08-29 15:49:45 -0300

    Update and rename sharkgostoso2.0.js to bot3.js

--- bot3.js
+++ bot3.js
@@ -1,6 +1,6 @@
 /**
- *Copyright 2016 SharkGostoso
- *Foda-se.
+ *Copyright 2015 basicBot
+ *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
  */
 
