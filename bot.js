57bbc80517cd66205b3c85efa3dd9c74f5a729c3
Kenan-plugdj <marlons.r.davila@gmail.com>
2016-10-09 20:12:13 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -305,7 +305,7 @@
             fbLink: "http://bit.ly/2cxnE6a",
             youtubeLink: null,
             website: null,
-            intervalMessages: ["@djs Vejam as regras digitando !regras, see the rules typing !rules.","Entre para o nosso grupo no Facebook http://bit.ly/2cxnE6a","Tema: Rock'n'roll, todos os subgêneros do rock.","Veja os comandos do bot digitando !cmds."],
+            intervalMessages: ["@djs Vejam as regras digitando !regras, see the rules typing !rules.","Entre para o nosso grupo no Facebook http://bit.ly/2cxnE6a","Tema: Rock'n'roll, todos os subgêneros do rock.","Veja os comandos do bot digitando !cmds.","@djs Temos um grupo da sala no Whatsapp, você pode entrar usando o link de convite que está na descrição (acessível clicando no nome da sala) ou no nosso grupo do facebook."],
             messageInterval: 3,
             songstats: false,
             commandLiteral: "!",
