5fc0de8113917e6d8a3dad317a435c146d93905b
Kenan-plugdj <marlons.r.davila@gmail.com>
2016-12-05 03:06:48 -0200

    Update bot.js
    Mudança de hierarquia dos comandos.

--- bot.js
+++ bot.js
@@ -1545,7 +1545,7 @@
 
             addCommand: {
                 command: 'add',
-                rank: 'mod',
+                rank: 'bouncer',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -2759,7 +2759,7 @@
 
             moveCommand: {
                 command: 'move',
-                rank: 'mod',
+                rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
